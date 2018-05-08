68dc0332a1b0db702d71d2a06b6411a6955d9632
k-calderon <bluelotus79@gmail.com>
2018-05-07 23:02:33 -0700

    finished filling out inner planets

---
+++ js/planets.js
@@ -0,0 +1,438 @@
+var innerPlanet = {
+    lType: [
+        {
+            type: "No Planet",
+            discoveryReward: 0,
+            rollRangeMin: 1,
+            rollRangeMax: 50
+        },
+        {
+            type: "Asteroid Belt",
+            discoveryReward: 100,
+            rollRangeMin: 51,
+            rollRangeMax: 60
+        },
+        {
+            type: "Ice World",
+            discoveryReward: 100,
+            rollRangeMin: 61,
+            rollRangeMax: 90
+        },
+        {
+            type: "Rocky",
+            discoveryReward: 200,
+            rollRangeMin: 91,
+            rollRangeMax: 100
+        }        
+    ],
+    mType: [        
+        {
+            type: "No Planet",
+            discoveryReward: 0,
+            rollRangeMin: 1,
+            rollRangeMax: 40
+        },
+        {
+            type: "Asteroid Belt",
+            discoveryReward: 100,
+            rollRangeMin: 41,
+            rollRangeMax: 50
+        },
+        {
+            type: "Ice World",
+            discoveryReward: 100,
+            rollRangeMin: 51,
+            rollRangeMax: 60
+        },
+        {
+            type: "Rocky",
+            discoveryReward: 200,
+            rollRangeMin: 61,
+            rollRangeMax: 65
+        },
+        {
+            type: "High Metal Content",
+            discoveryReward: 700,
+            rollRangeMin: 66,
+            rollRangeMax: 85
+        },
+        {
+            type: "Metal Rich",
+            discoveryReward: 2000,
+            rollRangeMin: 86,
+            rollRangeMax: 95
+        },
+        {
+            type: "Rocky",
+            goldilocksType: "Water World",
+            discoveryReward: 200,
+            goldilocksReward: 10000,
+            rollRangeMin: 91,
+            rollRangeMax: 100
+        },
+        {
+            type: "High Metal Content",
+            goldilocksType: "Earth-Like",
+            discoveryReward: 700,
+            goldilocksReward: 20000,
+            rollRangeMin: 91,
+            rollRangeMax: 99
+        },
+        {
+            type: "Metal Rich",
+            goldilocksType: "Ammonia World",
+            discoveryReward: 700,
+            goldilocksReward: 5000,
+            rollRangeMin: 100,
+            rollRangeMax: 100
+        }        
+    ],
+    kType: [
+        {
+            type: "No Planet",
+            discoveryReward: 0,
+            rollRangeMin: 1,
+            rollRangeMax: 30
+        },
+        {
+            type: "Asteroid Belt",
+            discoveryReward: 100,
+            rollRangeMin: 31,
+            rollRangeMax: 40
+        },
+        {
+            type: "Ice World",
+            discoveryReward: 100,
+            rollRangeMin: 41,
+            rollRangeMax: 41
+        },
+        {
+            type: "Rocky",
+            discoveryReward: 200,
+            rollRangeMin: 42,
+            rollRangeMax: 50
+        },
+        {
+            type: "High Metal Content",
+            discoveryReward: 700,
+            rollRangeMin: 51,
+            rollRangeMax: 75
+        },
+        {
+            type: "Metal Rich",
+            discoveryReward: 2000,
+            rollRangeMin: 76,
+            rollRangeMax: 85
+        },
+        {
+            type: "Rocky",
+            goldilocksType: "Water World",
+            discoveryReward: 200,
+            goldilocksReward: 10000,
+            rollRangeMin: 86,
+            rollRangeMax: 95
+        },
+        {
+            type: "High Metal Content",
+            goldilocksType: "Earth-Like",
+            discoveryReward: 700,
+            goldilocksReward: 20000,
+            rollRangeMin: 96,
+            rollRangeMax: 99
+        },
+        {
+            type: "Metal Rich",
+            goldilocksType: "Ammonia World",
+            discoveryReward: 700,
+            goldilocksReward: 5000,
+            rollRangeMin: 100,
+            rollRangeMax: 100
+        }
+    ],
+    gType: [
+        {
+            type: "No Planet",
+            discoveryReward: 0,
+            rollRangeMin: 1,
+            rollRangeMax: 30
+        },
+        {
+            type: "Asteroid Belt",
+            discoveryReward: 100,
+            rollRangeMin: 31,
+            rollRangeMax: 40
+        },
+        {
+            type: "Ice World",
+            discoveryReward: 100,
+            rollRangeMin: 41,
+            rollRangeMax: 41
+        },
+        {
+            type: "Rocky",
+            discoveryReward: 200,
+            rollRangeMin: 42,
+            rollRangeMax: 50
+        },
+        {
+            type: "High Metal Content",
+            discoveryReward: 700,
+            rollRangeMin: 51,
+            rollRangeMax: 60
+        },
+        {
+            type: "Metal Rich",
+            discoveryReward: 2000,
+            rollRangeMin: 61,
+            rollRangeMax: 80
+        },
+        {
+            type: "Rocky",
+            goldilocksType: "Water World",
+            discoveryReward: 200,
+            goldilocksReward: 10000,
+            rollRangeMin: 81,
+            rollRangeMax: 90
+        },
+        {
+            type: "High Metal Content",
+            goldilocksType: "Earth-Like",
+            discoveryReward: 700,
+            goldilocksReward: 20000,
+            rollRangeMin: 91,
+            rollRangeMax: 99
+        },
+        {
+            type: "Metal Rich",
+            goldilocksType: "Ammonia World",
+            discoveryReward: 700,
+            goldilocksReward: 5000,
+            rollRangeMin: 100,
+            rollRangeMax: 100
+        }
+    ],
+    fType: [
+        {
+            type: "No Planet",
+            discoveryReward: 0,
+            rollRangeMin: 1,
+            rollRangeMax: 40
+        },
+        {
+            type: "Asteroid Belt",
+            discoveryReward: 100,
+            rollRangeMin: 41,
+            rollRangeMax: 50
+        },
+        {
+            type: "Ice World",
+            discoveryReward: 100,
+            rollRangeMin: 51,
+            rollRangeMax: 51
+        },
+        {
+            type: "Rocky",
+            discoveryReward: 200,
+            rollRangeMin: 52,
+            rollRangeMax: 60
+        },
+        {
+            type: "High Metal Content",
+            discoveryReward: 700,
+            rollRangeMin: 61,
+            rollRangeMax: 70
+        },
+        {
+            type: "Metal Rich",
+            discoveryReward: 2000,
+            rollRangeMin: 71,
+            rollRangeMax: 85
+        },
+        {
+            type: "Rocky",
+            goldilocksType: "Water World",
+            discoveryReward: 200,
+            goldilocksReward: 10000,
+            rollRangeMin: 86,
+            rollRangeMax: 90
+        },
+        {
+            type: "High Metal Content",
+            goldilocksType: "Earth-Like",
+            discoveryReward: 700,
+            goldilocksReward: 20000,
+            rollRangeMin: 91,
+            rollRangeMax: 95
+        },
+        {
+            type: "Metal Rich",
+            goldilocksType: "Ammonia World",
+            discoveryReward: 700,
+            goldilocksReward: 5000,
+            rollRangeMin: 96,
+            rollRangeMax: 96
+        },
+        {
+            type: "Star",
+            discoveryReward: 0,
+            rollRangeMin: 97,
+            rollRangeMax: 100
+        }
+    ],
+    aType: [
+        {
+            type: "No Planet",
+            discoveryReward: 0,
+            rollRangeMin: 1,
+            rollRangeMax: 50
+        },
+        {
+            type: "Asteroid Belt",
+            discoveryReward: 100,
+            rollRangeMin: 51,
+            rollRangeMax: 55
+        },
+        {
+            type: "Ice World",
+            discoveryReward: 100,
+            rollRangeMin: 56,
+            rollRangeMax: 60
+        },
+        {
+            type: "Rocky",
+            discoveryReward: 200,
+            rollRangeMin: 61,
+            rollRangeMax: 65
+        },
+        {
+            type: "High Metal Content",
+            discoveryReward: 700,
+            rollRangeMin: 66,
+            rollRangeMax: 70
+        },
+        {
+            type: "Metal Rich",
+            discoveryReward: 2000,
+            rollRangeMin: 71,
+            rollRangeMax: 90
+        },
+        {
+            type: "Rocky",
+            goldilocksType: "Water World",
+            discoveryReward: 200,
+            goldilocksReward: 10000,
+            rollRangeMin: 91,
+            rollRangeMax: 91
+        },
+        {
+            type: "High Metal Content",
+            goldilocksType: "Earth-Like",
+            discoveryReward: 700,
+            goldilocksReward: 20000,
+            rollRangeMin: 92,
+            rollRangeMax: 92
+        },
+        {
+            type: "Metal Rich",
+            goldilocksType: "Ammonia World",
+            discoveryReward: 700,
+            goldilocksReward: 5000,
+            rollRangeMin: 93,
+            rollRangeMax: 93
+        },
+        {
+            type: "Star",
+            discoveryReward: 0,
+            rollRangeMin: 94,
+            rollRangeMax: 100
+        }
+    ],
+    bType: [
+        {
+            type: "No Planet",
+            discoveryReward: 0,
+            rollRangeMin: 1,
+            rollRangeMax: 60
+        },
+        {
+            type: "Asteroid Belt",
+            discoveryReward: 100,
+            rollRangeMin: 61,
+            rollRangeMax: 65
+        },
+        {
+            type: "Ice World",
+            discoveryReward: 100,
+            rollRangeMin: 66,
+            rollRangeMax: 70
+        },
+        {
+            type: "Rocky",
+            discoveryReward: 200,
+            rollRangeMin: 71,
+            rollRangeMax: 75
+        },
+        {
+            type: "High Metal Content",
+            discoveryReward: 700,
+            rollRangeMin: 76,
+            rollRangeMax: 80
+        },
+        {
+            type: "Metal Rich",
+            discoveryReward: 2000,
+            rollRangeMin: 81,
+            rollRangeMax: 90
+        },        
+        {
+            type: "Star",
+            discoveryReward: 0,
+            rollRangeMin: 91,
+            rollRangeMax: 100
+        }
+    ],
+    oType: [
+        {
+            type: "No Planet",
+            discoveryReward: 0,
+            rollRangeMin: 1,
+            rollRangeMax: 65
+        },
+        {
+            type: "Asteroid Belt",
+            discoveryReward: 100,
+            rollRangeMin: 66,
+            rollRangeMax: 70
+        },
+        {
+            type: "Ice World",
+            discoveryReward: 100,
+            rollRangeMin: 71,
+            rollRangeMax: 75
+        },
+        {
+            type: "Rocky",
+            discoveryReward: 200,
+            rollRangeMin: 76,
+            rollRangeMax: 80
+        },
+        {
+            type: "High Metal Content",
+            discoveryReward: 700,
+            rollRangeMin: 81,
+            rollRangeMax: 85
+        },
+        {
+            type: "Metal Rich",
+            discoveryReward: 2000,
+            rollRangeMin: 86,
+            rollRangeMax: 90
+        },        
+        {
+            type: "Star",
+            discoveryReward: 0,
+            rollRangeMin: 91,
+            rollRangeMax: 100
+        }
+    ]
+};
