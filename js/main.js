b0138e92e755a0fa7bc9e24a239484ecd1b1d53d
k-calderon <bluelotus79@gmail.com>
2018-05-20 09:45:50 -0700

    cleanup

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 "use strict";
 
-function difference(a, b) {
-    return Math.abs(a-b);
-};
-
+const difference = (a, b) => Math.abs(a-b);
 function randIntBetween(min, max) {
     if(min > max) {
         var tempMin = max;
@@ -14,14 +11,8 @@ function randIntBetween(min, max) {
     and the max do not have to start with 1*/
     return Math.floor(Math.random()*(max-min+1)+min);
 };
-
-function d100() {
-    return randIntBetween(1, 100)    
-};
-function d10() {
-    return randIntBetween(1, 10)    
-};
-
+const d100 = () => randIntBetween(1, 100);
+const d10 = () => randIntBetween(1, 10);
 function rollD100 (overrideRoll) {
     var roll = 1; //initialize roll        
         if (overrideRoll >= 1 && overrideRoll <= 100){
@@ -31,7 +22,6 @@ function rollD100 (overrideRoll) {
         };
     return roll;
 };
-
 function rollD10 (overrideRoll) {
     var roll = 1; //initialize roll        
         if (overrideRoll >= 1 && overrideRoll <= 10){
@@ -41,10 +31,7 @@ function rollD10 (overrideRoll) {
         };
     return roll;
 };
-
 var starSystem = []; //Initializing starSystem
-
-
 var generate = {    
     star: function(overrideRoll) { //overrideRoll forces a certain d100() result instead of rolling the dice
         var roll = rollD100(overrideRoll);
@@ -90,7 +77,6 @@ var generate = {
         starSystem = []; //clears starSystem
         handlers.addStar(overrideRoll); //add the first star, which is the primary stars
         var potentialNewStar = generate.star(); //rolls a new star
-
         /*checks to see if the new star has less mass than the star previous to it. if it does,
         it is added to the starSystem, a new star is generated, and the cycle repeats until
         the new star has a higher mass than the last star generated*/
@@ -99,7 +85,7 @@ var generate = {
             potentialNewStar = generate.star();
         };
         generate.planets();
-        //generate.moons();
+        generate.moons();
         render.stars();
         render.planets();
     },
@@ -174,7 +160,6 @@ var generate = {
     }
 
 };
-
 var handlers = {  
     addStar: function(overrideRoll) {
         starSystem.push(generate.star(overrideRoll));        
